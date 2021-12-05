c48f32ef444ab98902c19c7ab438fd067e567d6b
KaZ <nicholasgom3z@hotmail.com>
2021-12-04 22:37:14 -0500

    Equipo totalmente completada 100% responsive y funcional

--- src/app/slt/pages/equipo/equipo.component.ts
+++ src/app/slt/pages/equipo/equipo.component.ts
@@ -17,20 +17,24 @@ export class EquipoComponent  {
   dropdown_classE : string = "dropdown-edwin no-display";
   ver_classJ : string = "mas";
   ver_classE : string = "mas";
-  
+  main_Class : string = "";
+  flex_classJ:string="";
+  flex_classE:string="";
 
   dropDownJuan(){
 
     this.dropdown_classJ = "dropdown-juan slide-in-right";
     this.ver_classJ = "no-display";
-
+    this.main_Class = "activado";
+    this.flex_classJ = "row-active";
   }
 
   dropDownEdwin(){
 
-    this.dropdown_classE = "dropdown-edwin slide-in-right";
+    this.dropdown_classE = "dropdown-edwin slide-in-left";
     this.ver_classE = "no-display";
-
+    this.main_Class = "activado";
+    this.flex_classE = "row-active";
   }
   
 
